da6de8a2f13bde2194681b75d1566a4294a4caba
james <james@medable.com>
2022-03-18 16:46:03 -0700

    don't pollute JavaScriptCompiler for all envs with Promises as return values for all calls to template()

--- index.js
+++ index.js
@@ -1,40 +1,44 @@
 const { registerCoreHelpers } = require('./helpers')
 
 function asyncHelpers(handlebars) {
+
   const _compile = handlebars.compile,
-        _template = handlebars.VM.template,
-        _mergeSource = handlebars.JavaScriptCompiler.prototype.mergeSource
+    _template = handlebars.VM.template
 
-  handlebars.JavaScriptCompiler.prototype.mergeSource = function(varDeclarations) {
-    const sources = _mergeSource.call(this, varDeclarations)
-    return sources.prepend('return (async () => {').add(' })()')
-  }
+  handlebars.JavaScriptCompiler = class extends handlebars.JavaScriptCompiler {
 
-  handlebars.JavaScriptCompiler.prototype.appendToBuffer = function(source, location, explicit) {
-    // Force a source as this simplifies the merge logic.
-    if (!Array.isArray(source)) {
-      source = [source]
+    mergeSource(varDeclarations) {
+      const sources = super.mergeSource(varDeclarations)
+      return sources.prepend('return (async () => {').add(' })()')
     }
-    source = this.source.wrap(source, location)
 
-    if (this.environment.isSimple) {
-      return ['return await ', source, ';']
-    } if (explicit) {
-      // This is a case where the buffer operation occurs as a child of another
-      // construct, generally braces. We have to explicitly output these buffer
-      // operations to ensure that the emitted code goes in the correct location.
-      return ['buffer += await ', source, ';']
+    appendToBuffer(source, location, explicit) {
+      // Force a source as this simplifies the merge logic.
+      if (!Array.isArray(source)) {
+        source = [source]
+      }
+      source = this.source.wrap(source, location)
+
+      if (this.environment.isSimple) {
+        return ['return await ', source, ';']
+      } if (explicit) {
+        // This is a case where the buffer operation occurs as a child of another
+        // construct, generally braces. We have to explicitly output these buffer
+        // operations to ensure that the emitted code goes in the correct location.
+        return ['buffer += await ', source, ';']
+      }
+      source.appendToBuffer = true
+      source.prepend('await ')
+      return source
+
     }
-    source.appendToBuffer = true
-    source.prepend('await ')
-    return source
 
   }
 
   handlebars.template = function(spec) {
     spec.main_d = (prog, props, container, depth, data, blockParams, depths) => async(context) => {
       // const main = await spec.main
-      const v = spec.main(container, context, container.helpers, container.partials, data, blockParams, depths)
+      return spec.main(container, context, container.helpers, container.partials, data, blockParams, depths)
       return v
     }
     return _template(spec, handlebars)
